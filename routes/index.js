ca364f724830e63a0c82384262ff213867ba6d94
wuyifan1995 <502397665@qq.com>
2018-12-13 17:21:32 +0800

    哟花

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,6 @@ router.get('/search', async function(req, res, next) {
 router.get('/shopDetail', async function(req, res, next) {
   var detail = (await request.get(`/api/getCommodityDetails?item_id=${req.query.item_id}`)).body;
   if (detail.data[0].small_images) {
-    console.log(detail.data[0])
     res.render('detail', { title: detail.data[0].title, detail: detail.data[0], simple: detail.data[0].small_images,});
   } else {
     var simple = (await request.get(`/api/taobao/CommodityDetails?num_iid=${req.query.item_id}`)).body;
