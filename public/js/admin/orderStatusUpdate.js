05506a29217165dc3572eae5620413d9163dbde2
wuyifan1995 <502397665@qq.com>
2019-01-02 16:15:23 +0800

    优化

--- public/js/admin/orderStatusUpdate.js
+++ public/js/admin/orderStatusUpdate.js
@@ -1,11 +1,16 @@
 $('.search-btn').on('click', function(){
+  var id = $('.order-status div.active').attr('data-id');
   $.myPutJSON({
     url: '/api/orderStatusUpdate',
     data: JSON.stringify({
       settle_time: $('.order-search input').val(),
+      status: id,
     }),
     success: function(res){
       console.log(res);
     }
   })
 });
+$('.order-status div').on('click', function(){
+  $(this).addClass('active').siblings().removeClass('active')
+})
